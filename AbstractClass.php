196951be794dd791eeaab5f5e459d85bd1738c02
BoengGit <edukasiasqor@gmail.com>
2022-11-07 08:24:34 +0700

    Abstract Class

---
+++ AbstractClass.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "data/Location.php";
+
+use Data\{Location, City, Country, Province};
+
+// $location = new Location(); ERROR
+
+$city = new City();
+$country = new Country();
+$provience = new Province();
